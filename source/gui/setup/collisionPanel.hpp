bf1c02fb02aecbe13140a429c067c014a5a9d5a5
Alexander Weinrauch <alexander.weinrauch@gmail.com>
2018-10-19 00:29:17 +0200

    Worked on the interface for controlling collision shapes

--- source/gui/setup/collisionPanel.hpp
+++ source/gui/setup/collisionPanel.hpp
@@ -6,8 +6,33 @@
 
 #include <memory>
 
+namespace priv
+{
+void addVector3(aw::gui::LinearContainer::SPtr& container, std::string title)
+{
+  using namespace aw::gui;
+  auto& gui = container->getGUI();
+
+  auto label = std::make_shared<Label>(gui, std::move(title));
+  label->setAlignmentH(AlignmentH::Center);
+  label->setPadding({5.f, 0.f, 2.f, 0.f});
+  container->addChild(label, 0);
+
+  auto subContainer = std::make_shared<LinearContainer>(gui, Orientation::Horizontal);
+  subContainer->setSpaceBetweenElements(3.f);
+  container->addChild(subContainer, 0);
+  auto x = std::make_shared<TextBox>(gui, "0.0");
+  subContainer->addChild(x);
+  auto y = std::make_shared<TextBox>(gui, "0.0");
+  subContainer->addChild(y);
+  auto z = std::make_shared<TextBox>(gui, "0.0");
+  subContainer->addChild(z);
+}
+} // namespace priv
+
 void setupCollisionPanel(const aw::MessageBus& messageBus, aw::gui::LinearContainer::SPtr& container)
 {
+  using namespace aw::gui;
   auto& gui = container->getGUI();
   auto panel = std::make_shared<aw::gui::Panel>(gui);
   panel->setPadding(aw::gui::Padding(10.f));
@@ -17,4 +42,8 @@ void setupCollisionPanel(const aw::MessageBus& messageBus, aw::gui::LinearContai
 
   auto addBtn = std::make_shared<aw::gui::Button>(gui, "Add rect");
   layout->addChild(addBtn, 0);
+
+  priv::addVector3(layout, "Position");
+  priv::addVector3(layout, "Scale");
+  priv::addVector3(layout, "Rotation");
 }
